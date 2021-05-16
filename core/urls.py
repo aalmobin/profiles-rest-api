fd11ad4a9030f3e4b3c249ac675f570b4155e7cd
Mahian <mdmahian580@gmail.com>
2021-05-16 20:30:22 +0600

    Added login API

--- core/urls.py
+++ core/urls.py
@@ -3,10 +3,11 @@ from .import views
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
-router.register('hello-viewset/', views.HelloViewSet, basename='hello-viewset')
+router.register('hello-viewset', views.HelloViewSet, basename='hello-viewset')
 router.register('profile', views.UserProfileViewSet)
 
 urlpatterns = [
     path('hello-view/', views.HelloApiView.as_view()),
+    path('login/', views.UserLoginApiView.as_view()),
     path('', include(router.urls)),
 ]
